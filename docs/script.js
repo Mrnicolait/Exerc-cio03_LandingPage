918f3e37fb40e02d760e3651bbf40064d41b6e8b
Mrnicolait <mrnicolait@gmail.com>
2024-07-31 16:00:34 -0300

    Exerc04 Refactor

--- docs/script.js
+++ docs/script.js
@@ -1,17 +1,38 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    const contactForm = document.getElementById('contact-form');
-
-    contactForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        alert('Obrigado por entrar em contato! Em breve responderemos sua mensagem.');
-        contactForm.reset();
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 5,
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"]
     });
 });
 
-function redirectToMembership() {
-    window.location.href = 'pagina_de_associacao.html'; 
+// Galeria de Fotos com Modal
+const galeriaImgs = document.querySelectorAll('#galeria img');
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('img-modal');
+const span = document.getElementsByClassName('close')[0];
+
+galeriaImgs.forEach(img => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalImg.src = img.src;
+    });
+});
+
+span.onclick = function() { 
+    modal.style.display = 'none';
 }
-function redirectToStore() {
-            window.location.href = 'loja.html';
-        }
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
